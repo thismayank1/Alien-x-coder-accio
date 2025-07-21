676b14eb51a0b49653a927e932d397062ad9823a
thismayank1 <mayank108108@gmail.com>
2025-07-21 16:11:08 +0530

    Renamed jdjd.js to datatype.js

---
+++ datatype.js
@@ -0,0 +1,26 @@
+
+
+a = 10 // Integer  =>Number
+b = 10.5 // Decimal  => Number
+c = 'Abhishek' // text => string 
+d = true // boolean 
+e = false // boolean
+
+// console.log(a, typeof(a))
+// console.log(b, typeof(b))
+// console.log(c, typeof(c))
+// console.log(d, typeof(d))
+// console.log(e, typeof(e))
+
+
+// a = 10
+
+
+
+a = '12'
+console.log(a, typeof(a))
+
+
+// a = 'a' 
+
+// console.log(a, typeof(a))   
