3ac88544e16b36a600db264406640e43f37e3088
thismayank1 <mayank108108@gmail.com>
2025-07-23 13:21:26 +0530

    add new file console.js

---
+++ 5-console.js
@@ -0,0 +1,37 @@
+
+// a = 100
+
+
+// console.log(a)
+
+// console.log("ram is dancing",a)
+
+
+// The value of a is 100
+
+// console.log("The value of a is",a)
+
+
+a = 11
+b = 20 
+sum = a+b
+// console.log("The sum of a and b is", sum)
+
+// The sum of 10 and 20 is 30
+
+// console.log("The sum of",a,"and",b,"is",sum)
+
+console.log(`The sum of ${a} and ${b} is ${sum}`)
+
+
+x = 100
+y = 5 
+div = x/y 
+
+// console.log(x/y)
+
+// divison of 10 / 5 is 2
+
+// console.log("divison of", x, "/" ,y , "is",div)
+
+// console.log(`divison of ${x} / ${y} is ${div}`)
